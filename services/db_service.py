5d4282a258ecbfb9e536b1ff12a2ef1bb3f33377
collegelogin <collegelogin000@gmail.com>
2026-08-26 23:32:31 +1200

    update databse spelling mistake of name
    make is updated to name

--- services/db_service.py
+++ services/db_service.py
@@ -41,7 +41,6 @@ class DatabaseService:
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS cars (
                 car_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                make TEXT NOT NULL,
                 model TEXT NOT NULL,
                 year INTEGER NOT NULL,
                 mileage INTEGER NOT NULL,
